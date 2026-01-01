3315e00e582027dfa8516534116fd981c92a9f1f
sokevinjonas <kjonasdevpro@gmail.com>
2026-01-01 22:17:07 +0000

    correction de probleme lieer a l'endpoint qui permet de clore ujn tournoi et distribuer les recompenses

--- database/migrations/2025_12_18_113826_create_transactions_table.php
+++ database/migrations/2025_12_18_113826_create_transactions_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid')->unique();
+            $table->uuid('uuid')->unique()->nullable();
             $table->foreignId('wallet_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->enum('type', ['credit', 'debit']);
             $table->decimal('amount', 10, 2);
             $table->decimal('balance_before', 10, 2);
             $table->decimal('balance_after', 10, 2);
-            $table->enum('reason', ['initial_bonus', 'tournament_registration', 'tournament_prize', 'refund', 'admin_adjustment', 'tournament_entry_received', 'tournament_entry_refunded']);
+            $table->enum('reason', ['initial_bonus', 'tournament_registration', 'tournament_prize', 'refund', 'admin_adjustment', 'tournament_entry_received', 'tournament_entry_refunded', 'tournament_profit']);
             $table->string('description')->nullable();
             $table->foreignId('tournament_id')->nullable()->constrained()->onDelete('set null');
             $table->timestamps();
